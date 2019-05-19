25d7f4fa3dc3a86c79e48c136394cb2f84f3e07f
trie168 <trie168@gmail.com>
2019-05-19 11:24:53 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam porro optio dolorem aspernatur fugit, sequi, sapiente voluptatibus necessitatibus excepturi, error culpa incidunt cumque illo inventore debitis asperiores? Amet, iusto sit.</p>
 </body>
 </html>
